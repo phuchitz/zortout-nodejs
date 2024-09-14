151c995c9f8dca7c6056000f04734744a107dc85
Phuchit Kerdthenchai <phu.kchai@gmail.com>
2024-09-14 21:39:11 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -7,9 +7,10 @@ const mongoURI = process.env.MONGO_URI
 const connectDB = async () => {
     try {
         await mongoose.connect(mongoURI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            //useNewUrlParser: true,
+            //useUnifiedTopology: true,
         });
+        console.log("try con db");
         logger.info("Connected to MongoDB");
     } catch (e) {
         logger.error(`Error connecting to MongoDB: ${e.message}`);
